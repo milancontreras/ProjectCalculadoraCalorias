8ea09ed9502561414d8be34571005e95eba45c1e
milancontreras <milan.contreras@epn.edu.ec>
2021-01-17 11:46:58 -0500

    Commit and push #8

--- src/main/java/ec/edu/epn/tdd/Project/Execute.java
+++ src/main/java/ec/edu/epn/tdd/Project/Execute.java
@@ -3,12 +3,14 @@ package ec.edu.epn.tdd.Project;
 public class   Execute {
     public static  void main (String [] args){
 
-        System.out.println("Commit and push #7");
+        System.out.println("Commit and push #8");
 
         Persona personaM = new Persona(1,"Boris","Caiza",56,161,20,1,"M");
         Calorias calorías0 = new Calorias(personaM);
 
-        System.out.println("Calorias: "+calorías0.calcularCalorias());
+
+        System.out.println(personaM.personaGrafico());
+        System.out.println("\nLa cantidad calorica que se debe consumir por dia es de: "+calorías0.calcularCalorias()+" Kcal");
 
 
     }
