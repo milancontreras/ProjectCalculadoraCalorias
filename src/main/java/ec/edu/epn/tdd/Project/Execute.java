99ca807157965a75bea8fcda02b55fec866adca8
milancontreras <milan.contreras@epn.edu.ec>
2021-01-17 14:20:39 -0500

    cambio 13

--- src/main/java/ec/edu/epn/tdd/Project/Execute.java
+++ src/main/java/ec/edu/epn/tdd/Project/Execute.java
@@ -3,14 +3,19 @@ package ec.edu.epn.tdd.Project;
 public class   Execute {
     public static  void main (String [] args){
 
-        System.out.println("Cambio #12");
+        System.out.println("Cambio #13");
 
         Persona personaM = new Persona(1,"Boris","Caiza",56,161,20,1,"M");
         Calorias calorías0 = new Calorias(personaM);
+        Dieta dieta = new Dieta(calorías0, personaM);
+        System.out.println("\n=====================================================");
 
-
+        //Imprimir datos de la persona
         System.out.println(personaM.personaGrafico());
         System.out.println("\nLa cantidad calorica que se debe consumir por dia es de: "+calorías0.calcularCalorias()+" Kcal");
+        System.out.println("\n=====================================================");
 
+        //Imprimir las calorias que se pueden consumir por comida
+        dieta.imprimirCaloriasDietaPorComida();
     }
 }
