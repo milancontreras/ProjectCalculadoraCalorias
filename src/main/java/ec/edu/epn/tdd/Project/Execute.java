e8e87a1923f6f0f450910eeab78f84b6743b7168
milancontreras <milan.contreras@epn.edu.ec>
2021-01-17 10:52:35 -0500

    Commit and push #6

--- src/main/java/ec/edu/epn/tdd/Project/Execute.java
+++ src/main/java/ec/edu/epn/tdd/Project/Execute.java
@@ -3,7 +3,7 @@ package ec.edu.epn.tdd.Project;
 public class   Execute {
     public static  void main (String [] args){
 
-        System.out.println("Commit and push #4");
+        System.out.println("Commit and push #6");
 
         Persona personaM = new Persona(1,"Boris","Caiza",56,161,20,1,"M");
         Calorias calorías0 = new Calorias(personaM);
